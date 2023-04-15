e83e88ed60b40bf5264f855ceb7153917503a6d8
Daniel Durán <danieldurange@outlook.com>
2023-04-15 18:44:59 -0500

    05-correciones

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,7 @@
 export const getGifs = async (category) => {
   // console.log(import.meta.env.VITE_GIPHY_KEY);
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=BcCbyS9lsFEA0xVCXEpzSK50RnxEova1&q=${category}&limit=10`;
+  const giphyKey = "Aqui debes ingresar tu key de la api Giphy";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
